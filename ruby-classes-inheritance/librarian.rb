f996189b7b71a54c1479d36f59ec84aa749e8aed
kolegran <kolegran@gmail.com>
2018-08-13 19:28:22 +0300

    Checking the existence of a book is simplified

--- ruby-classes-inheritance/librarian.rb
+++ ruby-classes-inheritance/librarian.rb
@@ -19,32 +19,28 @@ class Librarian < Person
   def find_book_by_title(library, title)
     found_book = (library.books.find { |book| title == book.title })
 
-    book_issue(found_book)
+    book_exist(found_book)
   end
 
   def find_book_by_author(library, author)
     found_book = (library.books.find { |book| author == book.author })
 
-    book_issue(found_book)
+    book_exist(found_book)
   end
 
   def find_book_by_year(library, year)
     found_book = (library.books.find { |book| year == book.year })
 
-    book_issue(found_book)
+    book_exist(found_book)
   end
 
-  def book_issue(found_book)
-    if (book_exist?(found_book))
+  def book_exist(found_book)
+    if (found_book)
       found_book.status = 'issued'
 
       return found_book
     else
-      raise "Book isn\’t exist"
+      raise "Book isn\'t exist!"
     end
-  end
-
-  def book_exist?(found_book)
-    found_book ? found_book : nil
   end
 end
