dfd2ff9c25dc78b99a6e715e241bdbd30755f580
kolegran <kolegran@gmail.com>
2018-09-04 05:03:12 +0300

    Left one version of parsing

--- struct-parsing/parse.rb
+++ struct-parsing/parse.rb
@@ -7,11 +7,38 @@ json = JSON.parse(file)
 
 info_hash = json["info"]
 
-Movie  = Struct.new(:movie_hash)
-p test = Movie.new(info_hash["movie"])
+def form_attr(curr_hash)
+  curr_hash.keys.map { |k| k.to_sym }
+end
 
-Rating  = Struct.new(:rating_hash)
-p test = Rating.new(info_hash["rating"])
+def attr_values(curr_hash)
+  curr_hash.values
+end
 
-Actor  = Struct.new(:actor_arr)
-p test = Actor.new(info_hash["actor"])
+def arr_of_attr_values(curr_arr)
+  actors = []
+  curr_arr.each { |curr_hash| actors << attr_values(curr_hash) }
+
+  actors
+end
+
+# MovieClass
+movie_attr = form_attr(info_hash["movie"])
+movie_attr_val = attr_values(info_hash["movie"])
+
+Movie = Struct.new(*movie_attr)
+p movie_obj = Movie.new(*movie_attr_val)
+
+# RatingClass
+rating_attr = form_attr(info_hash["rating"])
+rating_attr_val = attr_values(info_hash["rating"])
+
+Rating = Struct.new(*rating_attr)
+p rating_obj = Rating.new(*rating_attr_val)
+
+# ActorClass
+actor_attr = form_attr(info_hash["actor"].first)
+actors_attr_val = arr_of_attr_values(info_hash["actor"])
+
+Actor = Struct.new(*actor_attr)
+actors_attr_val.each { |sub_arr| p Actor.new(*sub_arr) }
